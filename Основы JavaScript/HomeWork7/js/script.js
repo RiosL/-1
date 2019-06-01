3bd89e2f4b86e03d3920ffcc5f277346fadbcaf8
Anton <weather51nomad.com>
2019-06-01 18:32:34 +0800

    add Timer

--- Основы JavaScript/HomeWork7/js/script.js
+++ Основы JavaScript/HomeWork7/js/script.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
     let tab = document.querySelectorAll('.info-header-tab'),
-        info = document.querySelectorAll('.info-header'),
+        info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
     function hideTabContent(a) {
@@ -16,8 +16,8 @@ window.addEventListener('DOMContentLoaded', function() {
 
     function showTabContent(b) {
         if (tabContent[b].classList.contains('hide')) {
-            tabContent[b].classList.remove('show');
-            tabContent[b].classList.add('hide');
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
         }
     }
 
@@ -34,4 +34,64 @@ window.addEventListener('DOMContentLoaded', function() {
         }
 
     });
+
+
+
+    // Timer
+
+    let deadline = '2019-06-02';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/1000/60/60) % 24);
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        timer.childNodes.forEach(function(item) {
+            if (item.nodeType == 1) {
+                item.style.fontFamily = 'monospace';
+            }
+        });
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            function addZero(num) {
+                if (num < 10) {
+                    return '0' + num;
+                } else {
+                    return num;
+                } 
+            }
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';   
+            }
+        }
+
+    }
+
+    setClock('timer', deadline);
 });
