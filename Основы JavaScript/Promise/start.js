b9ede207582a0de2209cce4194120a453f0232ab
Anton <weather51nomad.com>
2019-06-28 22:36:27 +0800

    Обработка слайдов

---
+++ Основы JavaScript/Promise/start.js
@@ -0,0 +1,36 @@
+let drink = 1;
+
+function shoot(arrow) {
+    console.log("Вы сделали выстрел...");
+
+	let promise = new Promise(function(resolve, reject) {
+		setTimeout(function(){
+			Math.random() > 0.5 ? resolve({}) : reject("ВЫ промахнулись");
+		}, 3000)
+	});
+	
+	return promise;
+
+};
+
+function win() {
+	console.log('Вы победили');
+	(drink == 1) ? buyBeer() : giveMoney();
+}
+
+function buyBeer() {
+	console.log('Вам купили пиво');
+}
+
+function giveMoney() {
+	console.log('Вам купили пиво');
+}
+
+function loose() {
+	console.log('Вы проиграли!');
+}
+
+shoot({})
+		.then(mark => console.log('Вы попали в цель'))
+		.then(win)
+		.catch(loose)
